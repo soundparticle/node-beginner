93df4c1bb88f1522b80454d3140afa4a5bec1e62
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-05 11:40:55 -0700

    added a route

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var http = require('http');
+var url = require("url");
 
-function onRequest(request, response) {
-    response.writeHead(200, {'content-type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function start(route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+        
+        response.writeHead(200, {'content-type': 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
-http.createServer(onRequest).listen(8888);
+exports.start = start;

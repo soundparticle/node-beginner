53e58d0113b13cfb5b5de0412a182b7a9efa639a
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-06 16:51:45 -0700

    tutorial done

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,9 @@
 // var exec = require("child_process").exec;
 var querystring = require("querystring"),
     fs = require("fs");
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
@@ -26,12 +27,27 @@ function start(response, postData) {
     // });
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent text: "+
-    querystring.parse(postData).text);
-    response.end();
+
+     var form = new formidable.IncomingForm();
+     console.log("about to parse");
+     form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+
+         /* Possible error on Windows systems:
+            tried to rename to an already existing file */
+        fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+            if(error) {
+                fs.unlink("/tmp.test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        response.writeHead(200, {"Content-Type": "/test/html"});
+        response.write("received image:<br/>");
+        response.write("img src='/show' />");
+        response.end();
+     });
 }
 
 function show(response) {

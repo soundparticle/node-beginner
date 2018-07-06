bab61d684fad8da92bff0dd3ec7360ecf194a29c
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-05 18:37:02 -0700

    how not to

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-var http = require('http');
+var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(pathname);
-        
         response.writeHead(200, {'content-type': 'text/plain'});
-        response.write('Hello World');
+        var content = route(handle, pathname)
+        response.write(content);
         response.end();
     }
     http.createServer(onRequest).listen(8888);
